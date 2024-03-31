df85189df6a38e2b9069cc9a3dce2c4480e417bb
shibla-hb <shahushibu05@gmail.com>
2024-03-31 19:22:16 +0300

    updated

--- customers/models.py
+++ customers/models.py
@@ -8,13 +8,14 @@ class Product(models.Model):
     customer_name = models.CharField(max_length=200)
     address = models.FloatField()
     place = models.FloatField()
-    person_name=models.FloatField()
-    person_designation=models.FloatField()
-    status=models.CharField(max_length=100)
+    contact_person=models.FloatField()
+    cp_designation=models.CharField(max_length=200)
+    cp_id=models.FloatField()
+    cp_nationality=models.CharField(max_length=100)
     priority = models.IntegerField(default=0)
     delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.title
+        return self.customer_name
