3059eab1e80b6e3e561865952399e8e609abef4c
shibla-hb <shahushibu05@gmail.com>
2024-03-31 19:25:37 +0300

    changes updated

--- project/settings.py
+++ project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates', 'static'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
